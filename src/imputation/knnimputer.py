938e547025d82e7789a7650e2ba9a84819bd9f45
Antoine Maïstre-Rice <antoinemaitre06@gmail.com>
2025-10-02 09:16:34 +0200

    refactor: use values instead of to_numpy()
    Co-authored-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- src/imputation/knnimputer.py
+++ src/imputation/knnimputer.py
@@ -20,7 +20,7 @@ class KnnImputer(Imputer):
                     self.ml_data.dataset_descriptor.input_column,
                     self.ml_data.dataset_descriptor.target_column,
                 ]
-            ].values
+            ].to_numpy()
         )
         imputed_df[self.ml_data.dataset_descriptor.target_column] = imputed_values[:, 1]
 
