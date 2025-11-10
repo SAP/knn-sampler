6b1856cc09809eab70e1d2c1a91a7877630c2069
Parastoo Pashmchi <153714449+Paras270@users.noreply.github.com>
2025-11-10 18:01:37 +0100

    Update src/imputation/knnxkde.py
    Co-authored-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- src/imputation/knnxkde.py
+++ src/imputation/knnxkde.py
@@ -177,7 +177,7 @@ class KNNxKDE:
         all_miss_patterns = np.unique(np.isnan(miss_data), axis=0)
         cells_distrib = dict()  # store (weights, values)
 
-        for n, current_miss_pattern in enumerate(all_miss_patterns):
+        for _n, current_miss_pattern in enumerate(all_miss_patterns):
             if not np.logical_or.reduce(
                 current_miss_pattern
             ):  # if there is no missing value
